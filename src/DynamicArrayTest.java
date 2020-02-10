a4a99ec20b9bddca895a6205f01eb62b05ea536f
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2020-02-10 15:24:46 -0300

    Implementa mais testes unitários na classe DynamicArray

--- src/DynamicArrayTest.java
+++ src/DynamicArrayTest.java
@@ -7,19 +7,8 @@ class DynamicArrayTest {
 
     private DynamicArray list;
 
-
-
-    @Test
-    void construtorVazio() {
-        list = new DynamicArray();
-        assertEquals(16, list.size());
-    }
-
     @Test
     void construtorNaoVazio() {
-        list = new DynamicArray(10);
-        assertEquals(10, list.size());
-
         list = new DynamicArray(0);
         assertEquals(0, list.size());
         assertTrue(list.isEmpty());
@@ -35,11 +24,19 @@ class DynamicArrayTest {
     }
 
     @Test
-    void clear() {
+    void clearArray() {
+        list = new DynamicArray();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        list.clear();
+        assertEquals(0, list.size());
+        assertEquals(-1, list.get(2));
     }
 
     @Test
-    void add() {
+    void addSemIndex() {
         list = new DynamicArray();
 
         list.add(1);
@@ -54,26 +51,120 @@ class DynamicArrayTest {
     }
 
     @Test
-    void testAdd() {
+    void testAddComIndex() {
+        list = new DynamicArray();
+
+        list.add(1);
+        list.add(2);
+        list.add(3, 0);
+        list.add(4, 2);
+
+        assertEquals(3, list.get(0));
+        assertEquals(4, list.get(2));
+        assertEquals(4, list.size());
     }
 
     @Test
     void indexOf() {
+        list = new DynamicArray();
+        list.add(1);
+        list.add(2);
+        list.add(4);
+
+        assertEquals(0, list.indexOf(1));
+        assertEquals(2, list.indexOf(4));
+        assertEquals(-1, list.indexOf(10));
     }
 
     @Test
     void contains() {
+        list = new DynamicArray();
+
+        list.add(1);
+        list.add(32);
+        list.add(3);
+
+        assertTrue(list.contains(1));
+        assertTrue(list.contains(32));
+        assertFalse(list.contains(4));
     }
 
     @Test
     void set() {
+        list = new DynamicArray();
+
+        list.add(1);
+        list.add(2);
+
+        list.set(4, 0);
+
+        assertEquals(4, list.get(0));
+        assertEquals(2, list.size());
     }
 
     @Test
     void size() {
+        list = new DynamicArray();
+        assertEquals(0, list.size());
+
+        list.add(1);
+        assertEquals(1, list.size());
+
+        for(int i = 0; i < 5; i++) {
+            list.add(i);
+        }
+        assertEquals(6, list.size());
+
+        list.remove(2);
+
+        assertEquals(5, list.size());
     }
 
     @Test
     void isEmpty() {
+        list = new DynamicArray();
+        assertTrue(list.isEmpty());
+
+        list.add(211);
+
+        assertFalse(list.isEmpty());
+        list.remove(0);
+
+        assertTrue(list.isEmpty());
+
+    }
+
+    @Test
+    void remove() {
+        list = new DynamicArray();
+
+        list.add(21);
+        list.add(42);
+
+        list.remove(0);
+
+        assertEquals(42, list.get(0));
+        assertEquals(1, list.size());
+
+        list.remove(1);
+        System.out.println(list.get(0));
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    void removeAt() {
+        list = new DynamicArray();
+
+        list.add(1);
+        list.add(1000);
+        list.add(4242);
+
+        list.removeAt(1000);
+
+        assertEquals(4242, list.get(1));
+        assertEquals(2, list.size());
+
+        list.removeAt(1);
+        assertEquals(4242, list.get(0));
     }
 }
